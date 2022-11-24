472652bd9b1a8bc02413841cc66a1ea2ff0ec665
francescoforlani <francescoforlani98@hotmail.it>
2022-11-24 18:23:44 +0100

    File aggiornato

--- tombola.py
+++ tombola.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 22 17:25:19 2022
+Modificato on Thu Oct 25 12:15:54 2022
+-Aggiunta  libreria Argparse con relativa richiesta. 
+-Aggiunta variabile nel ciclo For, [104-115], per ottenere i risultati della tombola una singola volta
+ e non per ogni giocatore.
+-Eliminazione globals  
 
-@author: Franc
+@author: Orlando
 
 """
 
@@ -12,54 +16,79 @@ import random
 from busta import busta
 from gruppo_cartelle import gruppo_di_cartelle
 from giocatore import giocatore
-from cartella import Cartella
+# from cartella import Cartella 
+from argparse import ArgumentParser
+from tabella import tabella 
+
+
+parser = ArgumentParser()
+
+parser.add_argument('-g', '--numero_giocatori',
+                    help='Numero dei giocatori',
+                    type=int, default=3)
+parser.add_argument('-n', '--numero_cartelle',
+                    help='numero carelle assegnate per giocatore',
+                    nargs='*', type=int, default=[3,2,1])
+parser.add_argument('-f', '--nomi_giocatori',
+                    help='nomi dei giocatori',
+                    action="store_true",
+                    default=['primo','secondo','terzo'])
 
 
 
 
-#inserisci numero giocatori (int<10)
-numero_giocatori = int(input("Ciao! Inserisci il numero di giocatori (numero intero, esempio: 2): "))
-if numero_giocatori > 10:
-    print(f"{numero_giocatori} giocatori sono troppi, il massimo è 10.")
-    sys.exit()
+args = parser.parse_args()
+numero_giocatori = args.numero_giocatori
+numero_cartelle = args.numero_cartelle
+nomi_giocatori = args.nomi_giocatori
 
-#inserisci nomi dei giocatori (lista) e controlla di ricevere tanti nomi quanti sono i giocatori
-nomi_giocatori = input(f"Ok siete in {numero_giocatori} giocatori. Inserisci i vostri nomi (esempio: Francesco Orlando): ")
-nomi_giocatori = nomi_giocatori.split()
-for i in range(len(nomi_giocatori)):
-    nomi_giocatori[i] = str(nomi_giocatori[i])
+
+# Controllo dati inseriti 
     
 if len(nomi_giocatori) != numero_giocatori:
     print('Il numero di giocatori non corrisponde alla lunghezza della lista dei nomi.')
     sys.exit()
 
-#inserisci il numero di cartelle per ogni giocatore (max 6) 
-cartelle_per_giocatore = [0]*numero_giocatori
-for i in range(len(nomi_giocatori)):
-    cartelle_per_giocatore[i] = input(f"Quante cartelle per {nomi_giocatori[i]}? Inserisci un numero: ")
+if numero_giocatori > 10:
+    print(f"{numero_giocatori} giocatori sono troppi, il massimo è 10.")
+    sys.exit()    
 
-for i in range(len(cartelle_per_giocatore)):
-    cartelle_per_giocatore[i] = int(cartelle_per_giocatore[i])
-    
-for i in range(len(cartelle_per_giocatore)):
-    if cartelle_per_giocatore[i] > 6:
-        print(f"{cartelle_per_giocatore[i]} cartelle sono troppe, il massimo per ogni giocatore è 6.")
+if  numero_giocatori != len(numero_cartelle):
+    print('Il numero di giocatori non corrisponde al numero di cartelle assegnate.')
+    sys.exit()    
+
+
+for i in range(len(numero_cartelle)):
+    if numero_cartelle[i] > 6:
+        print(f"{numero_cartelle[i]} cartelle sono troppe, il massimo per ogni giocatore è 6.")
         sys.exit()
         
 
 #assegna le cartelle richieste da ogni giocatore
 lista_giocatori = []
+
+    
 for i in range(len(nomi_giocatori)):
     gruppo_cartelle = gruppo_di_cartelle()
-    globals()["giocatore_"+str(i+1)] = giocatore(f"{nomi_giocatori[i]}", 
+    lista_giocatori1 = ["giocatore_"+str(i+1)]
+    lista_giocatori1 = giocatore(f"{nomi_giocatori[i]}", 
                                                  random.sample(gruppo_cartelle.crea_gruppo_cartelle(), 
-                                                               cartelle_per_giocatore[i]))
-    lista_giocatori.append(globals()["giocatore_"+str(i+1)])
-    print("Ecco le cartelle per ogni giocatore:")
-    print(vars(globals()["giocatore_"+str(i+1)]))
+                                                               numero_cartelle[i]))
+    lista_giocatori.append(lista_giocatori1)
+    print(f"Il giocatore {nomi_giocatori[i]} ha {numero_cartelle[i]} cartelle.")
+    
     
 #crea l'oggetto busta da cui estrarre i numeri
 busta = busta()
+# creazione oggetto tabellone
+tabellone=tabella()
+# niente_segno = 0
+segno = 0
+
+#Tabellone
+p = tabellone.valori_tab
+print('Tabellone:')
+print(f'{p}')
 
 #inizia l'estrazione dei numeri e il controllo delle cartelle man mano che escono i numeri
 print("Iniziamo. Premi INVIO per estrarre un numero.")
@@ -67,23 +96,58 @@ while True:
     input("")
     numero_estratto = busta.estraggo()
     print(f"il numero estratto è {numero_estratto}")
-        
-    #aggiorna le cartelle dei giocatori
+# agg Tabellone
+
+
+    tabellone.aggiungi_numero(numero_estratto)
+    sommrg = tabellone.sommanum_riga
+    sommct = tabellone.sommanum_cartella
+    b = tabellone.posizione_riga
+    g = tabellone.posizione_colonna
+    if sommrg == 2 and segno == 0:
+            print(f'Il tabellone ha fatto ambo alla riga:  {b+1}')
+            print(p[b])
+            segno += 1
+    elif sommrg == 3 and segno == 1:
+            print(f'Il tabellone ha fatto terno alla riga:  {b+1}')
+            print(p[b])
+            segno += 1
+    elif sommrg == 4 and segno == 2:
+            print(f'Il tabellone ha fatto quaterna alla riga:  {b+1}')
+            print(p[b])
+            segno += 1
+    elif sommrg == 5 and segno == 3:
+            print(f'Il tabellone ha fatto cinquina alla riga : {b+1}')
+            print(p[b])
+            segno += 1
+    elif sommct == 15 :
+            print(f'Il tabellone ha fatto Tombola nella cartella : {g+1}')
+            print(p[3*g:3*(g+1)])
+            sys.exit()
+            
+#aggiorna le cartelle dei giocatori
     lista_risultati_del_turno = []
+
     for giocatore in lista_giocatori:
         risultato = giocatore.controlla_risultato(numero_estratto)
         lista_risultati_del_turno.append(risultato)
         if risultato != "niente":
             print(f"{giocatore.nome} ha fatto {risultato}")
-        
+                
+
             
 
     # se qualcuno ha fatto tombola termina il gioco
-    exist_count = lista_risultati_del_turno.count("tombola")
-    if exist_count > 0: 
-        print("")
-        print("E' stata fatta tombola. Fine del gioco.")
-        sys.exit()
+            exist_count = lista_risultati_del_turno.count("tombola")
+            if exist_count > 0: 
+                print("")
+                print(f"E' stata fatta tombola da {giocatore.nome}")
+                sys.exit()
         
     print("Premi INVIO per estrarre un nuovo numero.")
+
+        
+        
+        
+        
 
