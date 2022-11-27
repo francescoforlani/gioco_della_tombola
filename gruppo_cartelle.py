e5db2358bf58069278bda6acc82b5a7dc5beba0f
Orlando Maerini <83587631+OrlandoMaerini@users.noreply.github.com>
2022-11-27 11:00:32 +0100

    Add files via upload

--- gruppo_cartelle.py
+++ gruppo_cartelle.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 29 18:28:57 2022
-@author: Francesco & Orlando
+
+@author: Franc
 """
 
-
+import numpy as np
 import random
 import sys
 from cartella import Cartella
@@ -16,8 +17,6 @@ class gruppo_di_cartelle:
     def __init__(self):
         pass
         
-    
-    
     def crea_gruppo_cartelle(self):
         # Metodo che restituisce una lista di 6 oggetti di tipo cartella (appunto un gruppo di cartelle)
         
